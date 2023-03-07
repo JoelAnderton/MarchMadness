34c0d52a93d9056fb0921d5a45bc9cce12b91d90
Joel Anderton <JOA48@pitt.edu>
2023-03-07 14:20:08 -0500

    Add in teams.

--- MarchMadness/MarchMadness_BracketPicker.py
+++ MarchMadness/MarchMadness_BracketPicker.py
@@ -10,7 +10,7 @@ import random
 def march_madness(matches):
     round_winners = []
     for match in matches:
-        flips = [random.choice(['H', 'T']) for i in range(23)]
+        flips = [random.choice(['H', 'T']) for i in range(33)]
         #print(match)
         #print(flips)
         num_H = 0
@@ -44,25 +44,61 @@ def march_madness(matches):
 
 # West Matches
 first_four_west = [{'Rutgers': 11, 'Notre Dame': 11}]
-matches_west = [{'Gonzaga': 1, 'Gorgia St': 16},
-                {'Boise St': 8, 'Memphis': 9},
+ff_west_winner = march_madness(first_four_west)
+matches_west = [{'Gonzaga': 1, 'Gorgia St.': 16},
+                {'Boise St.': 8, 'Memphis': 9},
                 {'UConn': 5, 'New Mexico': 12},
                 {'Arkansas': 4, 'Vermont': 13},
-                {'Alabama': 6, **ff_west_winner},
-                {'Texas Tech': 3, 'Montan St':14},
+                {'Alabama': 6, **ff_west_winner[1][0]},
+                {'Texas Tech': 3, 'Montan St.':14},
                 {'Michigan St': 7, 'Daivdson': 10},
                 {'Duke': 2, 'Cal St Fullerton': 15}]
 
-# First Four West Match
-ff_west_winner = march_madness(first_four_west)
-print('First Four West - Winner')
-print(ff_west_winner[0][0])
-ff_west_winner = ff_west_winner[1][0]
+# East Matches
+first_four_east = [{'Wyoming': 12, 'Indiana': 12}]
+ff_east_winner = march_madness(first_four_east)
+matches_east = [{'Baylor': 1, 'Norfolk': 16},
+                {'North Carolina': 8, 'Marquette': 9},
+                {'St. Marys': 5, **ff_east_winner[1][0]},
+                {'UCLA': 4, 'Akron': 13},
+                {'Texas': 6, 'Virginia Tech': 11},
+                {'Purdue': 3, 'Yale':14},
+                {'Murray St.': 7, ' San Francisco': 10},
+                {'Kentucky': 2, 'St. Peters': 15}]
+# South Matches
+first_four_south = [{'Wright St.': 16, 'Bryant': 16}]
+ff_south_winner = march_madness(first_four_south)
+matches_south = [{'Arizona': 1, **ff_south_winner[1][0]},
+                 {'Seton Hall': 8, 'TCU': 9},
+                 {'Huston': 5, 'UAB': 12},
+                 {'Illinois': 4, 'Chattanooga': 13},
+                 {'Colorado St.': 6, 'Michigan': 11},
+                 {'Tennessee': 3, 'Longwood':14},
+                 {'Ohio St': 7, 'Loyola': 10},
+                 {'Villanova': 2, 'Delaware': 15}]
 
+# Midwest Matches
+first_four_mw = [{'Texas Southern': 16, 'Texas A&M': 16}]
+ff_mw_winner = march_madness(first_four_mw)
+matches_mw = [{'Kansas': 1, **ff_mw_winner[1][0]},
+              {'San Diego St.': 8, 'Creighton': 9},
+              {'Iowa': 5, 'Richmond': 12},
+              {'Providence': 4, 'South Dakota St.': 13},
+              {'LSU': 6,'Iowa St.': 11},
+              {'Wisconsin': 3, 'Colgate':14},
+              {'USC': 7, 'Miami': 10},
+              {'Auburn': 2, 'Jacksonville St.': 15}]
+
+print('First Four - Winners:')
+print('West -', ff_west_winner[0][0])
+print('East -',ff_east_winner[0][0])
+print('South -',ff_south_winner[0][0])
+print('Midwest -',ff_mw_winner[0][0])
+ 
 ## Round 1
 round_1_winners = march_madness(matches_west)
 print()
-print('Round 2')
+print('Round 2:')
 for team in round_1_winners[0]:
     print(team)
  
@@ -73,7 +109,7 @@ round_2_matches = [{**round_1_winners[1][0], **round_1_winners[1][1]},
                    {**round_1_winners[1][6], **round_1_winners[1][7]}]
 round_2_winners = march_madness(round_2_matches)
 print()
-print('Round 3')
+print('Round 3:')
 for team in round_2_winners[0]:
     print(team)
 
@@ -82,7 +118,7 @@ round_3_matches = [{**round_2_winners[1][0], **round_2_winners[1][1]},
                    {**round_2_winners[1][2], **round_2_winners[1][3]}]
 round_3_winners = march_madness(round_3_matches)
 print()
-print('Round 4 - Elite 8')
+print('Round 4 - Elite 8:')
 for team in round_3_winners[0]:
     print(team)
     
@@ -90,7 +126,7 @@ for team in round_3_winners[0]:
 round_4_matches = [{**round_3_winners[1][0], **round_3_winners[1][1]}]
 round_4_winners = march_madness(round_4_matches)
 print()
-print('Round 5 - Final 4')
+print('Round 5 - Final 4:')
 for team in round_4_winners[0]:
     print(team)
 
