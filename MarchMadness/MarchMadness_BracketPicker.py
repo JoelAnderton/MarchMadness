2991d7c5cb4a40e2a7cfbed2d6c9b465678a4d07
Joel Anderton <JOA48@pitt.edu>
2023-03-09 12:30:42 -0500

    Add in code to print out the winners for each region.

--- MarchMadness/MarchMadness_BracketPicker.py
+++ MarchMadness/MarchMadness_BracketPicker.py
@@ -85,7 +85,7 @@ matches_east = [{'Baylor': 1, 'Norfolk': 16},
                 {'UCLA': 4, 'Akron': 13},
                 {'Texas': 6, 'Virginia Tech': 11},
                 {'Purdue': 3, 'Yale':14},
-                {'Murray St.': 7, ' San Francisco': 10},
+                {'Murray St.': 7, 'San Francisco': 10},
                 {'Kentucky': 2, 'St. Peters': 15}]
 # South Matches
 first_four_south = [{'Wright St.': 16, 'Bryant': 16}]
@@ -118,29 +118,61 @@ print('South -',ff_south_winner[0][0])
 print('Midwest -',ff_mw_winner[0][0])
  
 ## Round 1
-round_1_winners = march_madness(matches_west)
+round_1_winners_west = march_madness(matches_west)
+round_1_winners_east = march_madness(matches_east)
+round_1_winners_south = march_madness(matches_south)
+round_1_winners_mw = march_madness(matches_mw)
 print()
 print('Round 2:')
-print_winners('West', round_1_winners)
- 
+print_winners('West', round_1_winners_west)
+print_winners('East', round_1_winners_east)
+print_winners('South', round_1_winners_south)
+print_winners('MidWest', round_1_winners_mw)
+
 ## Round 2
-round_2_matches = round2(round_1_winners)
-round_2_winners = march_madness(round_2_matches)
+round_2_matches_west = round2(round_1_winners_west) 
+round_2_matches_east = round2(round_1_winners_east) 
+round_2_matches_south = round2(round_1_winners_south) 
+round_2_matches_mw = round2(round_1_winners_mw)
+round_2_winners_west = march_madness(round_2_matches_west)
+round_2_winners_east = march_madness(round_2_matches_east)
+round_2_winners_south = march_madness(round_2_matches_south)
+round_2_winners_mw = march_madness(round_2_matches_mw)
 print()
 print('Round 3:')
-print_winners('West', round_2_winners)
+print_winners('West', round_2_winners_west)
+print_winners('East', round_2_winners_east)
+print_winners('South', round_2_winners_south)
+print_winners('MidWest', round_2_winners_mw)
 
 ## Round 3
-round_3_matches = round3(round_2_winners)
-round_3_winners = march_madness(round_3_matches)
+round_3_matches_west = round3(round_2_winners_west)
+round_3_matches_east = round3(round_2_winners_east)
+round_3_matches_south = round3(round_2_winners_south)
+round_3_matches_mw = round3(round_2_winners_mw)
+round_3_winners_west = march_madness(round_3_matches_west)
+round_3_winners_east = march_madness(round_3_matches_east)
+round_3_winners_south = march_madness(round_3_matches_south)
+round_3_winners_mw = march_madness(round_3_matches_mw)
 print()
 print('Round 4 - Elite 8:')
-print_winners('West', round_3_winners)
+print_winners('West', round_3_winners_west)
+print_winners('East', round_3_winners_east)
+print_winners('South', round_3_winners_south)
+print_winners('MidWest', round_3_winners_mw)
     
 ## Round 4
-round_4_matches = round4(round_3_winners)
-round_4_winners = march_madness(round_4_matches)
+round_4_matches_west = round4(round_3_winners_west)
+round_4_matches_east = round4(round_3_winners_east)
+round_4_matches_south = round4(round_3_winners_south)
+round_4_matches_mw = round4(round_3_winners_mw)
+round_4_winners_west = march_madness(round_4_matches_west)
+round_4_winners_east = march_madness(round_4_matches_east)
+round_4_winners_south = march_madness(round_4_matches_south)
+round_4_winners_mw = march_madness(round_4_matches_mw)
 print()
 print('Round 5 - Final 4:')
-print_winners('West', round_4_winners)
-
+print_winners('West', round_4_winners_west)
+print_winners('East', round_4_winners_east)
+print_winners('South', round_4_winners_south)
+print_winners('MidWest', round_4_winners_mw)
