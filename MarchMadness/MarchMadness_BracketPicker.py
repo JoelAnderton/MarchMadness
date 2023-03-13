0c51ef8c10113c00700c93cf6167d32d2797b6e1
Joel Anderton <JOA48@pitt.edu>
2023-03-13 11:47:16 -0400

    Add check for at least one 5 seed vs. 12 seed upset.

--- MarchMadness/MarchMadness_BracketPicker.py
+++ MarchMadness/MarchMadness_BracketPicker.py
@@ -66,7 +66,7 @@ def print_winners(region, winners):
 
 
 stop_final_four = 0
-while stop_final_four <= 12:
+while stop_final_four <= 1:
     # West Matches
     first_four_west = [{'Rutgers': 11, 'Notre Dame': 11}]
     ff_west_winner = march_madness(first_four_west)
@@ -126,66 +126,69 @@ while stop_final_four <= 12:
     round_1_winners_east = march_madness(matches_east)
     round_1_winners_south = march_madness(matches_south)
     round_1_winners_mw = march_madness(matches_mw)
-    print()
-    print('Round 2:')
-    print_winners('West', round_1_winners_west)
-    print_winners('East', round_1_winners_east)
-    print_winners('South', round_1_winners_south)
-    print_winners('MidWest', round_1_winners_mw)
 
-    ## Round 2
-    round_2_matches_west = round2(round_1_winners_west)
-    round_2_matches_east = round2(round_1_winners_east)
-    round_2_matches_south = round2(round_1_winners_south)
-    round_2_matches_mw = round2(round_1_winners_mw)
-    round_2_winners_west = march_madness(round_2_matches_west)
-    round_2_winners_east = march_madness(round_2_matches_east)
-    round_2_winners_south = march_madness(round_2_matches_south)
-    round_2_winners_mw = march_madness(round_2_matches_mw)
-    print()
-    print('Round 3:')
-    print_winners('West', round_2_winners_west)
-    print_winners('East', round_2_winners_east)
-    print_winners('South', round_2_winners_south)
-    print_winners('MidWest', round_2_winners_mw)
+    # Check for at least one 5 seed vs. 12 seed upset
+    if  round_1_winners_west[0][2] == 'New Mexico' or  round_1_winners_east[0][2] == ff_east_winner[0][0] or round_1_winners_south[0][2] == 'UAB' or round_1_winners_mw[0][2] == 'Richmond':
+        print()
+        print('Round 2:')
+        print_winners('West', round_1_winners_west)
+        print_winners('East', round_1_winners_east)
+        print_winners('South', round_1_winners_south)
+        print_winners('MidWest', round_1_winners_mw)
 
-    ## Round 3
-    round_3_matches_west = round3(round_2_winners_west)
-    round_3_matches_east = round3(round_2_winners_east)
-    round_3_matches_south = round3(round_2_winners_south)
-    round_3_matches_mw = round3(round_2_winners_mw)
-    round_3_winners_west = march_madness(round_3_matches_west)
-    round_3_winners_east = march_madness(round_3_matches_east)
-    round_3_winners_south = march_madness(round_3_matches_south)
-    round_3_winners_mw = march_madness(round_3_matches_mw)
-    print()
-    print('Round 4 - Elite 8:')
-    print_winners('West', round_3_winners_west)
-    print_winners('East', round_3_winners_east)
-    print_winners('South', round_3_winners_south)
-    print_winners('MidWest', round_3_winners_mw)
+        ## Round 2
+        round_2_matches_west = round2(round_1_winners_west)
+        round_2_matches_east = round2(round_1_winners_east)
+        round_2_matches_south = round2(round_1_winners_south)
+        round_2_matches_mw = round2(round_1_winners_mw)
+        round_2_winners_west = march_madness(round_2_matches_west)
+        round_2_winners_east = march_madness(round_2_matches_east)
+        round_2_winners_south = march_madness(round_2_matches_south)
+        round_2_winners_mw = march_madness(round_2_matches_mw)
+        print()
+        print('Round 3:')
+        print_winners('West', round_2_winners_west)
+        print_winners('East', round_2_winners_east)
+        print_winners('South', round_2_winners_south)
+        print_winners('MidWest', round_2_winners_mw)
 
-    ## Round 4
-    round_4_matches_west = round4(round_3_winners_west)
-    round_4_matches_east = round4(round_3_winners_east)
-    round_4_matches_south = round4(round_3_winners_south)
-    round_4_matches_mw = round4(round_3_winners_mw)
-    round_4_winners_west = march_madness(round_4_matches_west)
-    round_4_winners_east = march_madness(round_4_matches_east)
-    round_4_winners_south = march_madness(round_4_matches_south)
-    round_4_winners_mw = march_madness(round_4_matches_mw)
-    print()
-    print('Round 5 - Final 4:')
-    print_winners('West', round_4_winners_west)
-    print_winners('East', round_4_winners_east)
-    print_winners('South', round_4_winners_south)
-    print_winners('MidWest', round_4_winners_mw)
+        ## Round 3
+        round_3_matches_west = round3(round_2_winners_west)
+        round_3_matches_east = round3(round_2_winners_east)
+        round_3_matches_south = round3(round_2_winners_south)
+        round_3_matches_mw = round3(round_2_winners_mw)
+        round_3_winners_west = march_madness(round_3_matches_west)
+        round_3_winners_east = march_madness(round_3_matches_east)
+        round_3_winners_south = march_madness(round_3_matches_south)
+        round_3_winners_mw = march_madness(round_3_matches_mw)
+        print()
+        print('Round 4 - Elite 8:')
+        print_winners('West', round_3_winners_west)
+        print_winners('East', round_3_winners_east)
+        print_winners('South', round_3_winners_south)
+        print_winners('MidWest', round_3_winners_mw)
 
-    # check that at least 1 team in the Final Four is a 1 seed and that the total of the Final Four seeds >=12
-    if round_4_winners_west[1][0][round_4_winners_west[0][0]] == 1 or round_4_winners_east[1][0][round_4_winners_east[0][0]] == 1 or round_4_winners_south[1][0][round_4_winners_south[0][0]] == 1 or round_4_winners_mw[1][0][round_4_winners_mw[0][0]] == 1:
-        stop_final_four = (round_4_winners_west[1][0][round_4_winners_west[0][0]] +
-                                round_4_winners_east[1][0][round_4_winners_east[0][0]] +
-                                round_4_winners_south[1][0][round_4_winners_south[0][0]] +
-                                round_4_winners_mw[1][0][round_4_winners_mw[0][0]])
+        ## Round 4
+        round_4_matches_west = round4(round_3_winners_west)
+        round_4_matches_east = round4(round_3_winners_east)
+        round_4_matches_south = round4(round_3_winners_south)
+        round_4_matches_mw = round4(round_3_winners_mw)
+        round_4_winners_west = march_madness(round_4_matches_west)
+        round_4_winners_east = march_madness(round_4_matches_east)
+        round_4_winners_south = march_madness(round_4_matches_south)
+        round_4_winners_mw = march_madness(round_4_matches_mw)
+        print()
+        print('Round 5 - Final 4:')
+        print_winners('West', round_4_winners_west)
+        print_winners('East', round_4_winners_east)
+        print_winners('South', round_4_winners_south)
+        print_winners('MidWest', round_4_winners_mw)
+
+        # check that at least 1 team in the Final Four is a 1 seed and that the total of the Final Four seeds >=12
+        if round_4_winners_west[1][0][round_4_winners_west[0][0]] == 1 or round_4_winners_east[1][0][round_4_winners_east[0][0]] == 1 or round_4_winners_south[1][0][round_4_winners_south[0][0]] == 1 or round_4_winners_mw[1][0][round_4_winners_mw[0][0]] == 1:
+            stop_final_four = (round_4_winners_west[1][0][round_4_winners_west[0][0]] +
+                                    round_4_winners_east[1][0][round_4_winners_east[0][0]] +
+                                    round_4_winners_south[1][0][round_4_winners_south[0][0]] +
+                                    round_4_winners_mw[1][0][round_4_winners_mw[0][0]])
 
 print('Done!', stop_final_four)
